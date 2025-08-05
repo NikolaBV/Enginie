ab50329ce5dc7c9bb46139831e3c80984a7308ba
Nikola Valkov <bulgarianmapper64@gmail.com>
2025-08-05 17:50:14 +0300

    Added a more readable deltaTime calculation

--- 2D Engine/2D Engine/Main.cpp
+++ 2D Engine/2D Engine/Main.cpp
@@ -2,13 +2,28 @@
 
 Game* game = nullptr;
 int main() {
+
+    const int FPS = 60;
+    const int FRAME_DELAY = 1000 / FPS;
+    Uint32 frameStart;
+    int frameTime;
+
     game = new Game();
     game->Init("First window", 640, 480, false);
 
     while (game->Running()) {
+        frameStart = SDL_GetTicks(); //How many milisecs since SDL was initilized
+
         game->HandleEvents();
         game->Update();
         game->Render();
+
+        frameTime = SDL_GetTicks() - frameStart; //How long did the game loop iteration (a frame) take 
+
+        //check if we need to delay running the next frame
+        if (FRAME_DELAY > frameTime) {
+            SDL_Delay(FRAME_DELAY - frameTime);
+        }
     }
 
     game->Clean();
